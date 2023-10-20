ea6cc44f953f7d0ec0e99647616ce9d6c4a4ba69
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-10-20 20:45:10 -0300

    fix: requisito 08

--- src/tests/FiltersAndSort.test.tsx
+++ src/tests/FiltersAndSort.test.tsx
@@ -1,6 +1,6 @@
 import { vi } from 'vitest';
 import { MockPlanets } from '../helper/MockPlanets';
-import { act, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { act, cleanup, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
@@ -8,22 +8,21 @@ describe('Testa se os filters e sort',()=>{
   beforeEach(() => {
     vi.spyOn(global, 'fetch')
     .mockResolvedValue({ok: true, json: async () => MockPlanets} as Response);
-
-   render(<App/>);
   }) 
   afterEach(() => {
     vi.clearAllMocks();
   });
+  // afterEach(cleanup);
 
   test('Testa se o name-filter filtra os planetas pelo nome', async ()=> {
-       const loading = screen.getByText('Loading...');
-     await waitForElementToBeRemoved(loading);
+    render(<App/>);
+    const loading = screen.getByText('Loading...');
+    await waitForElementToBeRemoved(loading);
 
-     expect(global.fetch).toBeCalledTimes(1);
+    expect(global.fetch).toBeCalledTimes(1);
      
-     const inputNameFilter = screen.getByTestId('name-filter');
+    const inputNameFilter = screen.getByTestId('name-filter');
     
-    act(async () => {
       await userEvent.type(inputNameFilter, 'o');
       const firstPlanetsFilter = screen.getAllByTestId('planet-name');
       expect(firstPlanetsFilter).toHaveLength(7);
@@ -31,10 +30,10 @@ describe('Testa se os filters e sort',()=>{
       await userEvent.type(inputNameFilter, "oo");
       const secondPlanetsFilter = screen.getAllByTestId('planet-name');
       expect(secondPlanetsFilter).toHaveLength(2);
-    })
   });
 
-  test('Testa se o column filter funciona', async ()=> {   
+  test('Testa se o column filter funciona', async ()=> { 
+    render(<App/>);  
     const loading = screen.getByText('Loading...');
     await waitForElementToBeRemoved(loading);
    
@@ -42,7 +41,7 @@ describe('Testa se os filters e sort',()=>{
 
     await userEvent.type(valueFilter, '1000');
     const planetsFilter = screen.getAllByTestId('planet-name');
-    expect(planetsFilter).toHaveLength(7);
+    expect(planetsFilter).toHaveLength(10);
     const btnRemoveFilters = screen.getByTestId('button-remove-filters');
     await userEvent.click(btnRemoveFilters);
     const planetsWithoutFilters = screen.getAllByTestId('planet-name');
@@ -51,6 +50,7 @@ describe('Testa se os filters e sort',()=>{
  });
 
  test('Testa se o btnSort funciona', async ()=> {   
+  render(<App/>);
   const loading = screen.getByText('Loading...');
   await waitForElementToBeRemoved(loading);
  
